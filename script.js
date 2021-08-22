1daede128763b925d60d5f30b6390bf721bf5423
fruityCode <89323544+fruityCode@users.noreply.github.com>
2021-08-21 20:11:40 -0400

    Added files
    Added html, css and js files to repo.

---
+++ script.js
@@ -0,0 +1,32 @@
+function calculateEGFR(age, gender, race, serumCreatinine) {
+  let egfr;
+  if (race == Black) {
+    if (gender == Male) {
+      if (serumCreatinine <= 0.9) {
+        egfr = 163 * ((serumCreatinine / 0.9) ** -0.411) * ((0.993)**age)
+      } else {
+        egfr = 163 * ((serumCreatinine / 0.9) ** -1.209) * ((0.993)**age)
+      }
+    } else {
+      if (serumCreatinine <= 0.7) {
+        egfr = 166 * ((serumCreatinine / 0.7) ** -0.329) * ((0.993)**age)
+      } else {
+        egfr = 166 * ((serumCreatinine / 0.7) ** -1.209) * ((0.993)**age)
+      }
+    }
+  } else {
+    if (gender == Male) {
+      if (serumCreatinine <= 0.9) {
+        egfr = 141 * ((serumCreatinine / 0.9) ** -0.411) * ((0.993)**age)
+      } else {
+        egfr = 141 * ((serumCreatinine / 0.9) ** -1.209) * ((0.993)**age)
+      }
+    } else {
+      if (serumCreatinine <= 0.7) {
+        egfr = 144 * ((serumCreatinine / 0.7) ** -0.329) * ((0.993)**age)
+      } else {
+        egfr = 144 * ((serumCreatinine / 0.7) ** -1.209) * ((0.993)**age)
+      }
+    }
+  }
+}
